9e51eb55cc6acac58e4e1358276e094e704f9ce8
sab-hub <126076983+sab-hub@users.noreply.github.com>
2023-03-06 17:07:36 +0100

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#pip install streamlit 
+
+
+# In[4]:
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+st.title("Dit is een titel")
+
+
+# In[ ]:
+
+
+
+
